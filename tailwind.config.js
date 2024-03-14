9a4607e6917a4e9b4597dfad49b4e1d295718b67
Mateus Drake <108342680+MateusKungo@users.noreply.github.com>
2024-03-14 11:00:10 +0100

    Cardapio

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage:{
+        "home":"url('/Cadarpio/assets/1734.jpg')"
+
+      }
+    },
+  },
+  plugins: [],
+}
+
